57c55ec0a1317990ebd3252625e387de2ea25532
Parham.mn <Modirniya@live.com>
2021-10-18 01:43:43 -0700

    Initial project commit

---
+++ src/main/kotlin/com/mindque/userdata/UserdataApplication.kt
@@ -0,0 +1,11 @@
+package com.mindque.userdata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserdataApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserdataApplication>(*args)
+}
